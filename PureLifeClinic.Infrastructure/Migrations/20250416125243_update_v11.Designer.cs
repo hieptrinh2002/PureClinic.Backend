152c7713f7e21c960d243262079932ecded369ac
unknown <hieptrinh2002@gmail.com>
2025-04-16 21:44:55 +0700

    Ignore appsettings files, update migration file

--- PureLifeClinic.Infrastructure/Migrations/20250416125243_update_v11.Designer.cs
+++ PureLifeClinic.Infrastructure/Migrations/20250416125243_update_v11.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PureLifeClinic.Infrastructure.Persistence.Data;
 
@@ -11,9 +12,11 @@ using PureLifeClinic.Infrastructure.Persistence.Data;
 namespace PureLifeClinic.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250416125243_update_v11")]
+    partial class update_v11
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
